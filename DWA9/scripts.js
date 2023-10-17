8ec1e0e7f009c3ca47b712fd688740b8694dbcfa
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-10-17 12:52:15 +0200

    completed DWA9

--- DWA9/scripts.js
+++ DWA9/scripts.js
@@ -28,21 +28,9 @@ const fragment = () =>{
   * @returns {ButtonElement} - button containing all details of specific book
   */
  
- const createPreview = ({author, id, image, title}) => {
-    const element = document.createElement('button')
- element.classList = 'preview'
-     element.setAttribute('data-preview', id)
-
-     element.innerHTML = `
-     <img
-    class="preview__image"
-    src="${image}"
-     />
-     <div class="preview__info">
-     <h3 class="preview__title">${title}</h3>
-     <div class="preview__author">${authors[author]}</div>
- </div>
-`  
+ const createPreview = (book) => {
+  const element = document.createElement("book-preview")
+element.setAttribute("data-book", JSON.stringify(book))
   return element
 } 
 
