4c5b0b76fbb2be2ea6b6a364c844abd2b05f7562
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-11-01 04:44:49 +0200

    added some styles

--- DWA14/script.js
+++ DWA14/script.js
@@ -3,7 +3,25 @@ import { LitElement, html, css } from "./litelement.js";
 class TallyApp extends LitElement {
   MAX_NUMBER = 15;
   MIN_NUMBER = -5;
-
+  static styles = css`
+    sl-button {
+      padding: 50px;
+      position: relative;
+      top: 90px;
+      font-size: 100px;
+    }
+    h1 {
+      text-align: center;
+      color: hsl(158, 60%, 48%);
+    }
+    sl-input {
+      width: 100vw;
+      position: relative;
+      top: 40px;
+      padding: 3% 10%;
+      font-size: 50px;
+    }
+  `;
   static properties = {
     count: { type: Number },
   };
@@ -15,37 +33,22 @@ class TallyApp extends LitElement {
 
   render() {
     return html`
-      <style>
-        .counter__value{
-
-          height:400px
-          weight: 400px   }
-
-          .head{
-            text-margin: center;
-          }
-      </style>
       <h1 class="head">Tally Count</h1>
-      <sl-input
-        class="counter__value"
-        data-key="number"
-        readonly
-        value=${this.count}
-        size="large"
+      <sl-input class="counter__value" data-key="number" readonly size="large"
         >${this.count}</sl-input
-      >
+      ><br />
       <sl-button
         @click=${this._addHandler}
         size="large"
-        disabled=${this.count === this.MAX_NUMBER}
-        variant="Primary"
+        disabled=${this.count >= this.MAX_NUMBER}
+        variant="primary"
         >+</sl-button
       >
       <sl-button
         @click=${this._subtractHandler}
         size="large"
-        disabled=${this.count === this.MIN_NUMBER}
-        variant="Primary"
+        disabled=${this.count <= this.MIN_NUMBER}
+        variant="primary"
         >-</sl-button
       >
     `;
@@ -53,16 +56,10 @@ class TallyApp extends LitElement {
 
   _subtractHandler() {
     this.count--;
-    this.count === this.MAX_NUMBER
-      ? console.log("Add button must be disabled")
-      : console.log(this.count);
   }
 
   _addHandler() {
     this.count++;
-    this.count === this.MAX_NUMBER
-      ? console.log("Add button must be disabled")
-      : console.log(this.count);
   }
 }
 
