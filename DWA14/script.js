1c36891dc6d206cd82494ac539300e1e12852fa0
Fhatuwani Mulaudzi <mulaudzifhatuwanib20@gmail.com>
2023-11-01 03:35:56 +0200

    added the increments and decrements

--- DWA14/script.js
+++ DWA14/script.js
@@ -1,32 +1,70 @@
-import { LitElement, html } from "./litelement";
+import { LitElement, html, css } from "./litelement.js";
 
 class TallyApp extends LitElement {
-  static style = `
-  body{
-        background-color: black
-    }`;
+  MAX_NUMBER = 15;
+  MIN_NUMBER = -5;
 
   static properties = {
-    state: { value: 0 },
+    count: { type: Number },
   };
 
   constructor() {
     super();
+    this.count = 0;
   }
 
   render() {
     return html`
+      <style>
+        .counter__value{
+
+          height:400px
+          weight: 400px   }
+
+          .head{
+            text-margin: center;
+          }
+      </style>
+      <h1 class="head">Tally Count</h1>
       <sl-input
         class="counter__value"
         data-key="number"
         readonly
-        value="0"
-      ></sl-input>
-      <sl-button variant="Default">+</sl-button>
-      <sl-button variant="Default">-</sl-button>
-      <sl-button variant="Default">Reset</sl-button>
+        value=${this.count}
+        size="large"
+        >${this.count}</sl-input
+      >
+      <sl-button
+        @click=${this._addHandler}
+        size="large"
+        disabled=${this.count === this.MAX_NUMBER}
+        variant="Primary"
+        >+</sl-button
+      >
+      <sl-button
+        @click=${this._subtractHandler}
+        size="large"
+        disabled=${this.count === this.MIN_NUMBER}
+        variant="Primary"
+        >-</sl-button
+      >
     `;
+  }
+
+  _subtractHandler() {
+    this.count--;
+    this.count === this.MAX_NUMBER
+      ? console.log("Add button must be disabled")
+      : console.log(this.count);
+  }
+
+  _addHandler() {
+    this.count++;
+    this.count === this.MAX_NUMBER
+      ? console.log("Add button must be disabled")
+      : console.log(this.count);
   }
 }
 
 customElements.define("tally-app", TallyApp);
+console.log(TallyApp);
